949dfde703706f938eb71960a58be95331eae788
alienshy <alisahverdiyeW8@gmail.com>
2023-12-19 16:28:10 +0400

    id

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 window.addEventListener("scroll",()=>{
     if(window.scrollY>100){
         document.querySelector("nav").style.backgroundColor = "rgba(255,255,255,1)"
-        document.querySelector("nav").a.style.color = "black"
+        document.querySelector("#a").style.color = "black"
+        document.querySelector("#sd").style.color = "black"
+        document.querySelector("#sf").style.color = "black"
+        document.querySelector("#sg").style.color = "black"
+        document.querySelector("#sh").style.color = "black"
+    
+        // document.querySelector("#a").style.color = "black"
     }
     else{
         document.querySelector("nav").style.backgroundColor = "rgba(0,0,0,0)"
-        document.querySelector("nav").style.color.a = "rgba()"
+        document.querySelector("#a").style.color = "white"
+        document.querySelector("#sd").style.color = "white"
+        document.querySelector("#sf").style.color = "white"
+        document.querySelector("#sg").style.color = "white"
+        document.querySelector("#sh").style.color = "white"
+       
     }
 })
